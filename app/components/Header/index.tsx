63ed8a3277ba742ac234dc807fefc278b0411183
CyberYin <cyberyin.me@gmail.com>
2024-07-08 17:57:33 +0800

    feat: 新增组件 (#7)

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,20 +1,86 @@
-import { Button } from "@nextui-org/react";
 import { useNavigate } from "@remix-run/react";
+import { LogOut, NotepadText, User } from "lucide-react";
 
-const Header: React.FC = () => {
+import { UserResponse } from "@/lib/types/user";
+
+import { Avatar, AvatarFallback, AvatarImage } from "../Avatar";
+import { Button } from "../Button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../Sheet";
+import { VisuallyHidden } from "../VisuallyHidden";
+
+const Header: React.FC<{
+  user?: UserResponse;
+}> = ({ user }) => {
   const navigate = useNavigate();
 
   return (
     <>
-      <header className="flex h-20 w-full items-center justify-between bg-primary px-4 shadow sm:px-6">
+      <header className="fixed z-20 flex h-16 w-full items-center justify-between bg-custom px-4 shadow sm:px-6">
         <h1 className="text-xl font-bold">酷客简历</h1>
-        <Button
-          onPressEnd={() => navigate("/sign-in")}
-          radius="sm"
-          className="bg-theme text-white"
-        >
-          登录
-        </Button>
+        {user ? (
+          <Sheet>
+            <SheetTrigger asChild>
+              <Avatar className="hover:cursor-pointer">
+                <AvatarImage src={user.avatar || ""} />
+                <AvatarFallback>
+                  {user.user_name.slice(0, 1).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            </SheetTrigger>
+            <SheetContent>
+              <VisuallyHidden asChild>
+                <SheetHeader>
+                  <SheetTitle></SheetTitle>
+                  <SheetDescription></SheetDescription>
+                </SheetHeader>
+              </VisuallyHidden>
+              <div className="flex items-center space-x-4">
+                <Avatar className="h-10 w-10">
+                  <AvatarImage src={user.avatar || ""} />
+                  <AvatarFallback>
+                    {user.user_name.slice(0, 1).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="font-semibold text-custom">
+                  {user.user_name}
+                </div>
+              </div>
+              <hr className="my-4 border-t-[1.5px] border-custom" />
+              <div className="space-y-1 text-custom">
+                <div
+                  onClick={() => {}}
+                  className="flex cursor-pointer items-center space-x-2 rounded-md px-4 py-1.5 transition-colors hover:bg-custom-hover"
+                >
+                  <User className="h-4 w-4" />
+                  <div className="text-sm">个人信息</div>
+                </div>
+                <div
+                  onClick={() => navigate("/dashboard")}
+                  className="flex cursor-pointer items-center space-x-2 rounded-md px-4 py-1.5 transition-colors hover:bg-custom-hover"
+                >
+                  <NotepadText className="h-4 w-4" />
+                  <div className="text-sm">我的简历</div>
+                </div>
+              </div>
+              <hr className="my-4 border-t-[1.5px] border-custom" />
+              <div className="space-y-1 text-custom">
+                <div className="flex cursor-pointer items-center space-x-2 rounded-md px-4 py-1.5 transition-colors hover:bg-custom-hover">
+                  <LogOut className="h-4 w-4" />
+                  <div className="text-sm">退出登录</div>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
+        ) : (
+          <Button onClick={() => navigate("/sign-in")}>登录</Button>
+        )}
       </header>
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,36 @@
-import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        theme: {
-          DEFAULT: "var(--color-theme-primary)",
-          primary: "var(--color-theme-primary)",
-          secondary: "var(--color-theme-secondary)",
+        primary: {
+          DEFAULT: "var(--color-primary-normal)",
+          light: "var(--color-primary-light)",
+          normal: "var(--color-primary-normal)",
+        },
+        danger: {
+          DEFAULT: "var(--color-danger-normal)",
+          light: "var(--color-danger-light)",
+          normal: "var(--color-danger-normal)",
+        },
+        warning: {
+          DEFAULT: "var(--color-warning-normal)",
+          light: "var(--color-warning-light)",
+          normal: "var(--color-warning-normal)",
+        },
+        success: {
+          DEFAULT: "var(--color-success-normal)",
+          light: "var(--color-success-light)",
+          normal: "var(--color-success-normal)",
+        },
+      },
+      borderColor: {
+        custom: {
+          DEFAULT: "var(--color-border-primary)",
+          primary: "var(--color-border-primary)",
+          secondary: "var(--color-border-secondary)",
         },
       },
       backgroundColor: {
@@ -20,6 +38,8 @@ export default {
           DEFAULT: "var(--color-background-primary)",
           primary: "var(--color-background-primary)",
           secondary: "var(--color-background-secondary)",
+          tertiary: "var(--color-background-tertiary)",
+          hover: "var(--color-background-hover)",
         },
       },
       textColor: {
@@ -27,10 +47,11 @@ export default {
           DEFAULT: "var(--color-font-primary)",
           primary: "var(--color-font-primary)",
           secondary: "var(--color-font-secondary)",
+          tertiary: "var(--color-font-tertiary)",
         },
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui(), require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
